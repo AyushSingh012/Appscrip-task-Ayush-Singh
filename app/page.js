3144e4e67bc5400e13315b96dd971898ca7ef3aa
Ayush Kumar Singh <ayushkumarsingh875@gmail.com>
2025-10-18 11:17:36 +0530

    add Hero Section

--- app/page.js
+++ app/page.js
@@ -1,15 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import HeroSection from '@/components/HeroSection';
 
 export default function Home() {
   return (
-    <main style={{ padding: '40px 20px', textAlign: 'center' }}>
-      <h1>DISCOVER OUR PRODUCTS</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus
-        scelerisque. Dolor integer scelerisque nibh amet mi ut elementum dolor.
-      </p>
+    <main>
+      <HeroSection />
     </main>
   );
 }
-
---
+++ components/HeroSection.js
@@ -0,0 +1,13 @@
+import './HeroSection.css';
+
+export default function HeroSection() {
+  return (
+    <section className="hero">
+      <h1>DISCOVER OUR PRODUCTS</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus
+        scelerisque. Dolor integer scelerisque nibh amet mi ut elementum dolor.
+      </p>
+    </section>
+  );
+}
